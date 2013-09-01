bc3a96f961a1680628395d5268e24b96a853f8a8
Andrew Cobb <cobb.andrew@gmail.com>
2013-08-31 19:54:45 -0500

    Adding back add_header_image shortcode.  Styling the biggest and smallest version of the header.

--- wp-content/themes/rr/functions.php
+++ wp-content/themes/rr/functions.php
@@ -393,6 +393,7 @@ remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altoget
 add_shortcode('html5_shortcode_demo', 'html5_shortcode_demo'); // You can place [html5_shortcode_demo] in Pages, Posts now.
 add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [html5_shortcode_demo_2] in Pages, Posts now.
 add_shortcode('contact_us', 'contact_us_func');
+add_shortcode('add_header_image', 'add_header_image_func');
 
 
 
@@ -481,6 +482,23 @@ function contact_us_func($atts, $content = null) {
 	return $html; 
 }
 
+//[add_header_image title="Personalized Content" excerpt="Transform your entire webpage in real time to reflect the true interests of your individual consumer." image_url="/wp-content/uploads/2013/08/solutions-personalization-bg1.jpg"]
+function add_header_image_func($atts, $content = null) {
+  extract( shortcode_atts( array(
+		'title' => 'Rich Relevance',
+		'excerpt' => '',
+		'image_url' => ''), $atts ) );
+	
+	$html = '<div class="slider">
+	          <div class="slider-content" style="background-image: url(' . $image_url . ')">
+    			  <h1 class="entry-title with-excerpt">' . esc_attr($title) . '</h1>
+    			  <p>' . esc_attr($excerpt) . '</p>
+          </div>
+          <div class="clear"></div></div>';
+
+	return $html;
+	//<img src="">
+}
 
 
 ?>
