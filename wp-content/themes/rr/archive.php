8881944e21b29c59a29494ce59bcb9ffae8513a0
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-02 12:55:15 -0500

    Adding archive header and styling

--- wp-content/themes/rr/archive.php
+++ wp-content/themes/rr/archive.php
@@ -1,24 +1,14 @@
-<?php 
-	get_header('blog'); 
-?>
-	
-	<!-- section -->
-	<section role="blog">
-	
-		<div style="max-width:1080px;margin:0 auto;">
-			<h1 style="margin-left:50px;margin-bottom:40px;"><?php _e( 'Archives', 'html5blank' ); ?></h1>
-	   		
-	   		<div class="all-posts" style="width:730px;float:left;">
-		   		<?php get_template_part('loop_blog'); ?>
-	   		</div>
-			<div class="posts-sidebar" style="float:left;width:300px;">
-			   <?php get_sidebar(); ?>
-			</div>
-	      	
-	         
-			
-		</div>      
-   </section>
-   <div class="clear" style="clear:both;"></div>
+<?php get_header(); ?>
 
+<section role="blog">
+	<div class="wrap">
+		<div class="all-posts">
+      <?php get_template_part('loop_blog'); ?>
+		</div>		
+		<div class="posts-sidebar">		   
+			<?php get_sidebar(); ?>		
+		</div>      	         			
+	</div>         
+</section>   
+<div class="clear" style="clear:both;"></div>
 <?php get_footer(); ?>
--- wp-content/themes/rr/header.php
+++ wp-content/themes/rr/header.php
@@ -333,15 +333,19 @@ $(".nav > ul > li > .sub-menu").removeClass("visible-sm");
 
 
 
-			<?php if( is_single()) { 
+			<?php if( is_single() || is_archive() ) { 
 			  //echo do_shortcode('[add_header_image title="Blog" image_url="/wp-content/uploads/2013/09/blog-bg.png"]'); 
 			  $blog_page_id = get_ID_by_slug('blog/rr-blog');
         $page_rr_blog = get_page($blog_page_id);
         $content = $page_rr_blog->post_content;
         echo apply_filters('the_content', $page_rr_blog->post_content); 
+        
+        // Check if search is_search()  
+        // Check if is_archive() 
+        
 			}?>
 			
-      <?php if ( !is_single() && !get_post_meta($post->ID, 'banner', true) && function_exists('easingsliderpro') ) { 
+      <?php if ( !is_single() && !is_archive() && !get_post_meta($post->ID, 'banner', true) && function_exists('easingsliderpro') ) { 
           echo '<div class="slider home">';
           easingsliderpro( 1 ); 
           echo '</div>';
