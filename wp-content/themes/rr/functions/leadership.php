9a184ade3060b6dd45d353e86e68c63749c0dde3
Kevin Toy <toyk008@gmail.com>
2013-09-02 09:17:16 -0500

    Adding wrap to section for leadership page.

--- wp-content/themes/rr/functions/leadership.php
+++ wp-content/themes/rr/functions/leadership.php
@@ -205,8 +205,10 @@ add_shortcode('leadership', 'create_leadership_func');
 
 function create_leadership_func($atts) {
   extract( shortcode_atts( array(
+    'class' => '',
 		'cat' => 'executive-team',
-		'bg' => 'white'), $atts ) );
+		'bg' => 'white',
+		'title' => ''), $atts ) );
 	
   $leadership = get_leadership($atts['cat']);
 	
@@ -242,7 +244,12 @@ function create_leadership_func($atts) {
 		}
 	}
 	
-	$content .= '<section role="leader" class="'. esc_attr($bg).'"><ul>' . $lis . '</ul></section><div class="clear"></div>';
+	$content .= '<section role="leader" class="'. esc_attr($class).' '. esc_attr($bg).'">
+	              <div class="wrap">
+	                <h3>'. esc_attr($title).'</h3>
+	                <ul>' . $lis . '</ul>
+	              </div>
+	              <div class="clear"></div></section>';
 	
 	/* $html = '<section role="'.$atts['role'].'" class="' . esc_attr($bg) . ' ' . esc_attr($align) . '" ><div class="wrap" >'.do_shortcode($content).'</div><div class="' . esc_attr($class) . '"></div></section>'; */
 	
