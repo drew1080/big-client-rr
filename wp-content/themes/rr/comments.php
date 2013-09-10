68d2369c32673eb68669c05ad1db9df405fd57a8
Kevin Toy <toyk008@gmail.com>
2013-09-10 09:33:59 -0500

    styling blog

--- wp-content/themes/rr/comments.php
+++ wp-content/themes/rr/comments.php
@@ -36,6 +36,12 @@ $args = array(
         ( $req ? '<span class="required">*</span>' : '' ) .
         '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
         '" size="30"' . $aria_req . ' /></p>',
+        
+       'url' =>
+        '<p class="comment-form-url"><label for="url">' .
+        __( 'Website', 'domainreference' ) . '</label>' .
+        '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+        '" size="30" /></p>'
       )
     )
 );
