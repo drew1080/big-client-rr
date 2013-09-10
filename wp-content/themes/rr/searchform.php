3ed0f69c2fe837336555697bb9f73733bc92d575
Andrew Cobb <cobb.andrew@gmail.com>
2013-09-09 19:09:37 -0500

    Removing search placeholder

--- wp-content/themes/rr/searchform.php
+++ wp-content/themes/rr/searchform.php
@@ -1,5 +1,5 @@
 <!-- search -->
 <form method="get" action="<?php echo home_url( '/' ); ?>" role="search">
-	<input class="search-input" type="search" name="s" placeholder="To search, type and hit enter.">
+	<input class="search-input" type="search" name="s">
 </form>
 <!-- /search -->
